31308fabe6a3caa59750bc9a015451beab58e07c
Michelle <michelleaqmaqm3@gmail.com>
2026-08-20 16:58:52 -0500

    Add files via upload

---
+++ Lab1/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce el tamaño del arreglo: ");
+        int n = scanner.nextInt();
+        System.out.println("Introduce los elementos del arreglo:");
+        int[] arreglo = new int[n];
+        leerArreglo(scanner, arreglo);
+        int sumaArreglo = sumarDatos(arreglo);
+        System.out.println("Suma del arreglo:" + sumaArreglo);
+    } 
+    public static void leerArreglo (Scanner scanner, int[] arreglo) {
+        for (int i=0; i < arreglo.length; i++) {
+            arreglo[i] = scanner.nextInt();
+        }
+    }
+    public static int sumarDatos (int[] arreglo) {
+        int suma = 0;
+        for (int num: arreglo) {
+            suma = suma + num;
+        }
+        return suma;
+    }
+}
