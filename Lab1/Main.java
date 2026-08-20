1e92033c525ab358a609330ce8e41d497107993e
Michelle <michelleaqmaqm3@gmail.com>
2026-08-20 17:39:15 -0500

    Add files via upload

---
+++ Lab1/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese horas: ");
+        int hora = scanner.nextInt();
+        System.out.print("Ingrese minutos: ");
+        int min = scanner.nextInt();
+        System.out.print("Ingrese segundos: ");
+        int seg = scanner.nextInt();
+        int convSegundos = conversion(hora, min, seg);
+        System.out.println("Conversion a segundos: "+convSegundos);
+    }
+    public static int conversion(int h, int m,int s) {
+        int totalseg;
+        totalseg = (h*3600) + (m*60) + s;
+        return totalseg;
+    }
+}
