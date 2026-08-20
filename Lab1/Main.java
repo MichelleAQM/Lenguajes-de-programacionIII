5f3624a61044c65d4135150e3063a31b9c1e87f6
Michelle <michelleaqmaqm3@gmail.com>
2026-08-20 17:27:09 -0500

    Add files via upload

---
+++ Lab1/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese numeros: ");
+        double n1 = scanner.nextDouble();
+        double n2 = scanner.nextDouble();
+        double n3 = scanner.nextDouble();
+        double menor = menorDeTres (n1, n2, n3);
+        System.out.println("Menar de tres:" + menor);
+    }
+    public static double menorDeTres (double n1, double n2, double n3) {
+        if (n1 <= n2 && n1 <=n3) {
+            return n1;
+        } 
+        else if (n2 <= n1 && n2 <=n3) {
+            return n2;
+        } else {
+            return n3;
+        }
+    }
+}
