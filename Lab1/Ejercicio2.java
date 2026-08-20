3ecd7b0ac33cc4bbfd170849eb886c6c3713700c
Michelle <michelleaqmaqm3@gmail.com>
2026-08-20 17:12:17 -0500

    Update and rename Main.java to Ejercicio2.java

--- Lab1/Ejercicio2.java
+++ Lab1/Ejercicio2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class Ejercicio2 {
     public static void main(String[] args) {
         Scanner scanner =  new Scanner(System.in);
         int[] arreglo = new int[10];
