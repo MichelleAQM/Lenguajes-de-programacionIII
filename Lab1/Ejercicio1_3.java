3b2a2fc1d5130f255733165d5dcc0a9ee422ef15
Michelle <michelleaqmaqm3@gmail.com>
2026-08-20 17:00:06 -0500

    Update and rename Main.java to Ejercicio1_3.java

--- Lab1/Ejercicio1_3.java
+++ Lab1/Ejercicio1_3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Main {
+class Ejercicio1_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce el tamaño del arreglo: ");
